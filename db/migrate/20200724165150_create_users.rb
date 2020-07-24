fb25338f7bae2289885fdfd86c77968ac8c01061
jundai.tanaka <jundai0130@gmail.com>
2020-07-24 16:55:02 +0000

    for heroku2

--- db/migrate/20200724165150_create_users.rb
+++ db/migrate/20200724165150_create_users.rb
@@ -1,12 +1,11 @@
-class AddUserToTasks < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tasks, :user, foreign_key: true
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password_digest
 
       t.timestamps
-    end  
+    end
   end
 end
