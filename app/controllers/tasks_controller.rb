ba6fa91b3c5bb4d70a3d4835202b831a987509d6
jundai.tanaka <jundai0130@gmail.com>
2020-07-26 09:19:39 +0000

    for heroku6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    
     def index
-        if logged_in?
-            @task = current_user.tasks.page(params[:page])
-        end    
+        @task = current_user.tasks.build
+        @tasks = current_user.tasks.page(params[:page])
     end
     
     def show
+        @task = current_user.tasks.find(params[:id])
     end
     
     def new
@@ -19,11 +20,12 @@ class TasksController < ApplicationController
         
         if @task.save
             flash[:success] = "タスクが正常に投稿されました"
-            redirect_to @task
+            redirect_to root_url
         
         else
-            flash.now[:danger] = "タスクが投稿されませんでした"
-            render :new
+            @microposts = current_user.tasks.order(id: :desc).page(params[:page])
+            flash.now[:danger] = 'タスクの投稿に失敗しました。'
+            render root_url
         end    
     end 
     
@@ -31,7 +33,6 @@ class TasksController < ApplicationController
     end
     
     def update
-        
         if @task.update(task_params)
             flash[:success] = "正常に更新されました"
             redirect_to @task
@@ -46,16 +47,19 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = "タスクは正常に削除されました"
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end 
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
-    
     def task_params
         params.require(:task).permit(:content, :status)
-    end    
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
 end
